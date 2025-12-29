8542b073ef8bfa0f11ffc1adaf0d7265043c3886
Vishnu Sharma <vishnusharma.11.2005@gmail.com>
2025-12-29 15:02:05 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,49 @@
 
+fetch("header.html")
+.then(res => res.text())
+.then(data => {
+	document.getElementById("header").innerHTML = data;
+    let currentPage = window.location.pathname.split("/").pop();
+    if ( !currentPage || currentPage == "#" ) {
+		currentPage = "index.html";
+    }
+    // menu me anchor tak pahuchne ka tareeka
+    let links = document.querySelectorAll(".menu .menubar ul li a");
+        
+	// hr anchor tag ko process karenge
+    links.forEach(link => {
+		//pehle  anchor tag ka href ki value save karenge
+    	let linkPage = link.getAttribute("href");
+		
+        if(!linkPage) return;
+  
+        if (linkPage == currentPage) {
+      		link.closest("li").classList.add("underline_current");
+    	}
+
+		if (window.innerWidth <= 576) {
+			const menu = document.querySelector(".menu");
+			const menuBar = document.querySelector(".menu .menubar ul");
+			const mobileBar = document.querySelector(".menu .mobilebar");
+
+			if(mobileBar && !mobileBar.querySelector(".hamburger")) {
+				const dv = document.createElement("div");
+				dv.className = "hamburger";
+				dv.setAttribute("aria-label", "Toggle menu");
+				dv.setAttribute("tabindex","0");
+				dv.innerHTML= '<span></span>';
+				mobileBar.appendChild(dv);
+
+				dv.addEventListener("click", function(e) {
+          
+					e.stopPropagation();
+					menu.classList.toggle("mobile-open");
+				});
+			}
+		}
+	});
+
+}) // end then
+.catch(err => {
+    console.error("Header load failed:", err);
+});
